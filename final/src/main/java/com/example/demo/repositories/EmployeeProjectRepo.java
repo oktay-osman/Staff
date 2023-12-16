57a589a25d10906ea0d342fe1c48cc77ffa08075
oktay-osman <oktai.osman21@gmail.com>
2023-12-16 23:02:20 +0200

    added EmployeeProject repository

---
+++ final/src/main/java/com/example/demo/repositories/EmployeeProjectRepo.java
@@ -0,0 +1,29 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.EmployeeProject;
+import com.example.demo.services.CSVReader;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeProjectRepo {
+    private JdbcTemplate jdbcTemplate;
+    private CSVReader reader = new CSVReader();
+
+    public EmployeeProjectRepo (JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void addEmployeeProject(EmployeeProject emp) {
+        String sql = "INSERT INTO employee_projects (employee_id, project_id, from_date, to_date) " +
+                "VALUES (?, ?, ? ,?)";
+        jdbcTemplate.update(sql, emp.getEmployeeId(), emp.getProjectId(), emp.getFromDate(), emp.getToDate());
+    }
+
+    public List<EmployeeProject> getAllEmployeeProjects() {
+        //TODO fix path and create run configuration
+        return (List<EmployeeProject>) reader.read("C:\\Users\\my\\IdeaProjects\\Staff\\Staff\\final\\src\\main\\resources\\static\\employee_projects.csv");
+    }
+}
