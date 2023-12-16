4b37fcc9c582b7652f6b622d3355e831d0265c35
oktay-osman <oktai.osman21@gmail.com>
2023-12-16 23:03:25 +0200

    added EmployeeProject service

--- final/src/main/java/com/example/demo/services/CSVReader.java
+++ final/src/main/java/com/example/demo/services/CSVReader.java
@@ -1,7 +1,6 @@
 package com.example.demo.services;
 
-import com.example.demo.models.EmployeeProjects;
-import org.springframework.cglib.core.Local;
+import com.example.demo.models.EmployeeProject;
 
 import java.io.*;
 import java.time.LocalDate;
@@ -11,7 +10,7 @@ import java.util.List;
 public class CSVReader implements Reader{
     @Override
     public List<? extends Serializable> read (String pathFile) {
-        List<EmployeeProjects> employeeProjects = new ArrayList<>();
+        List<EmployeeProject> employeeProjects = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader(pathFile))) {
             if(br.readLine().toLowerCase().contains("id")) {
                 br.readLine();
@@ -23,7 +22,7 @@ public class CSVReader implements Reader{
                         if(Integer.parseInt(values[0]) <= 0 || Integer.parseInt(values[1]) <= 0 ) {
                             br.readLine();
                         } else {
-                            employeeProjects.add(new EmployeeProjects(Long.parseLong(values[0]),
+                            employeeProjects.add(new EmployeeProject(Long.parseLong(values[0]),
                                     Long.parseLong(values[1]), LocalDate.parse(values[2]),
                                     values[3].isEmpty() || values[3].equals("NULL") ? null : LocalDate.parse(values[3])));
                         }
---
+++ final/src/main/java/com/example/demo/services/EmployeeProjectService.java
@@ -0,0 +1,25 @@
+package com.example.demo.services;
+
+import com.example.demo.models.EmployeeProject;
+import com.example.demo.repositories.EmployeeProjectRepo;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Service
+public class EmployeeProjectService {
+    private EmployeeProjectRepo employeeProjectRepo;
+
+    public EmployeeProjectService (EmployeeProjectRepo employeeProjectRepo) {
+        this.employeeProjectRepo = employeeProjectRepo;
+    }
+
+    public void addEmployeeProject(EmployeeProject emp) {
+        employeeProjectRepo.addEmployeeProject(emp);
+    }
+
+    public List<? extends Serializable> getAllEmployeeProjects() {
+        return employeeProjectRepo.getAllEmployeeProjects();
+    }
+}
